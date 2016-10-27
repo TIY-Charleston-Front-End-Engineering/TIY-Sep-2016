3b236d2d4b37989f0b67e4aa4a152c3d18dc1cd2
Travis Hubbard <t3patterson@gmail.com>
2016-10-27 14:06:30 -0400

    okay cool

--- code-examples/day-34-nodejs-environment/backbone-cards-in-node/src/scripts/app.js
+++ code-examples/day-34-nodejs-environment/backbone-cards-in-node/src/scripts/app.js
@@ -1,20 +1,88 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+var $ = require('jquery')
+var Backbone = require('Backbone')
 
-const app = function() {
+var UserCollection = require('./model-user.js')
+var ViewTemplateConstructor = require('./view-constructor.js')
+var cardsTemplateFn = require('./view-cards.js')
 
-	const Header = React.createClass({
-		render: () => {
-			return (
-				<div>
-					<h1>YOLO</h1>
-				   <a href="./about">Go To About Page</a>
-				</div>
-			)
-		}
-	})
+var AppRouter = Backbone.Router.extend({
+   routes: {
+      "nationality/:natty/gender/:gen" : "showOnlyGenderAndNatPage",
+      "gender/:gendr" : "showGenderPage",
+      "nationality/:natName": "showNatsPage",
+      "": "showHomePage"
+   },
 
-	ReactDOM.render(<Header/>,document.querySelector('.container'))
-}
+   showOnlyGenderAndNatPage: function(n, g){
+      var coll = new UserCollection("results=24&gender="+g+"&nat="+n)
+      console.log(coll.url)
+      coll.fetch().then(function(){
+         var view = new ViewTemplateConstructor('#app-container', cardsTemplateFn)
+         view.render(coll)
+      })
 
-app()
+   },
+
+   showGenderPage: function(genderType){
+      var coll = new UserCollection("results=24&gender="+genderType)
+      console.log(coll.url)
+      coll.fetch().then(function(){
+         var view = new ViewTemplateConstructor('#app-container', cardsTemplateFn)
+         view.render(coll)
+      })
+   },
+
+   showNatsPage: function(natty){
+      var coll = new UserCollection("results=24&nat="+natty)
+      console.log(coll.url)
+      coll.fetch().then(function(){
+         console.log(coll)
+         var view = new ViewTemplateConstructor('#app-container', cardsTemplateFn)
+         view.render(coll)
+      })
+
+   },
+
+   showHomePage: function(){
+      var coll = new UserCollection("results=24")
+      console.log(coll.url)
+
+      coll.fetch().then(function(){
+         console.log(coll)
+         var view = new ViewTemplateConstructor('#app-container', cardsTemplateFn)
+         view.render(coll)
+      })
+
+
+   },
+
+   // showPage: function(nat){
+   //    // var coll = new UserCollection(/*results=24&proper=query&string=required*/)
+   //    var qryStr = Backbone.history.getFragment()
+   //          .split("/").map(function(val, i){
+   //                if ( (i + 1) % 2 === 0){
+   //                   return  "=" + val
+   //                } else {
+   //                   return  "&" + val
+   //                }
+   //
+   //             })
+   //             .join('')
+   //
+   //    console.log('ROUTE: ', Backbone.history.getFragment())
+   //    console.log('queryString: ',qryStr)
+   //
+   //    var coll = new UserCollection("results=24"+qryStr)
+   //    coll.fetch().then(function(){
+   //         console.log(coll)
+   //         var view = new ViewTemplateConstructor('#app-container', cardsTemplateFn)
+   //         view.render(coll)
+   //    })
+   // },
+
+   initialize: function(){
+      Backbone.history.start()
+   }
+})
+
+var app = new AppRouter();
---
+++ code-examples/day-34-nodejs-environment/backbone-cards-in-node/src/scripts/model-user.js
@@ -0,0 +1,24 @@
+var Backbone = require('Backbone')
+var $ = require('jquery')
+
+
+var UserModel = Backbone.Model.extend({
+   idAttribute: 'email'
+   // initialize: function(bookId){
+   //    this.url = "path/to/user/"+userId
+   // }
+})
+
+
+var UserCollection = Backbone.Collection.extend({
+   model: UserModel,
+   parse: function(rawJSONRes){
+      return rawJSONRes.results
+   },
+   initialize: function(qryStr){
+      this.url = "https://randomuser.me/api?"+qryStr
+   }
+
+})
+
+module.exports = UserCollection
